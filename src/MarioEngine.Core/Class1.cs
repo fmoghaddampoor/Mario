5aa68a36f67c32d977c4339c483bd3cf479d0523
EROWA\farzad.moghaddampoor <farzad.moghaddampoor@erowa.com>
2026-06-01 20:04:18 +0200

    feat(core): add centralized Directory.Build.props and Directory.Packages.props
    - Directory.Build.props: shared properties, versioning, analyzers, code style
    - Directory.Packages.props: central NuGet version management
    - Clean up .csproj files (remove duplicated props inherited from build props)
    - Fix StyleCop compliance in Class1.cs files
    - Build succeeds with 0 warnings, 0 errors

--- src/MarioEngine.Core/Class1.cs
+++ src/MarioEngine.Core/Class1.cs
@@ -1,6 +1,9 @@
-﻿namespace MarioEngine.Core;
+// <copyright file="Class1.cs" company="MarioGame">
+// Copyright (c) MarioGame. All rights reserved.
+// </copyright>
+
+namespace MarioEngine.Core;
 
 public class Class1
 {
-
 }
--- src/MarioEngine.Game/Class1.cs
+++ src/MarioEngine.Game/Class1.cs
@@ -1,6 +1,9 @@
-﻿namespace MarioEngine.Game;
+// <copyright file="Class1.cs" company="MarioGame">
+// Copyright (c) MarioGame. All rights reserved.
+// </copyright>
+
+namespace MarioEngine.Game;
 
 public class Class1
 {
-
 }
